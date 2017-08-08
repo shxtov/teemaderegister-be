279c25cb2fc49830c2b6072b2c56fcc677b2dced
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-08-08 13:05:23 +0300

    added slugs to users

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
     profile: {
       firstName: { type: String, required: true },
       lastName: { type: String, required: true },
+      slug: { type: String, required: true, unique: true },
       image: { type: String }
     },
 
